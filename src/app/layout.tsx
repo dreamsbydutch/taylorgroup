373f25faf82f3869170673a1300ae89dd48a201a
dreamsbydutch <71857307+dreamsbydutch@users.noreply.github.com>
2025-02-14 15:59:54 -0500

    change client db error in prisma

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,7 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
+
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
