3fe0f0e2785e12c6b78f666ddefbc844c48c250e
Curtis Hough <chough14@gmail.com>
2025-02-17 18:10:43 -0500

    finally read next 15 docs for params

--- src/app/itemslist/[jobId]/page.tsx
+++ src/app/itemslist/[jobId]/page.tsx
@@ -1,9 +1,8 @@
 import ItemsList from "~/app/_components/ItemsList";
 
-export default async function ItemsListPage({
-  params,
-}: {
-  params: { jobId: string };
+export default async function ItemsListPage(props: {
+  params: Promise<{ jobId: string }>;
 }) {
+  const params = await props.params;
   return <ItemsList jobId={Number(params.jobId)} />;
 }
